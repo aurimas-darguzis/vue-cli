5e570a8ab1190e57a54edd415375d280f37b61a0
aurimas-darguzis <aurimas@darguzis.com>
2017-03-21 22:50:59 +0000

    code refactor

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
-  <h1>Hey!</h1>
+  <h1>Server Status: {{ status }}</h1>
   <hr>
 </template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        status: 'Critical'
+      }
+    } 
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-var components = {
-  data: function() {
-    return {
-      status: 'Critical'
-    }
-  },
-  template: '<p>Server Status: {{ status }}</p>'
-};
-
-// new Vue({
-//   el: '#app',
-//   components: {
-//     'my-comp': 'cmp'
-//     }
-// });
-
 new Vue({
   el: '#app',
   render: h => h(App)
