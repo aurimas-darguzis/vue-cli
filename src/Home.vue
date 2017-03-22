ee75d62bb5c2ae6eff0e0c44ec2bf61880e0771e
aurimas-darguzis <aurimas@darguzis.com>
2017-03-22 21:01:02 +0000

    added header component

--- src/Home.vue
+++ src/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
+        <app-header></app-header>
         <app-server-status v-for="server in 5"></app-server-status>
     </div>   
 </template>
 
 <script>
     import ServerStatus from './ServerStatus.vue';
+    import Header from './Header.vue';
 
     export default {
        components: {
-           'app-server-status': ServerStatus
+           'app-server-status': ServerStatus,
+           'app-header': Header
        }
     }
 </script>
