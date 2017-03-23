590d8d723ec11288b82fd306239d0713c7e2bf6e
aurimas-darguzis <aurimas@darguzis.com>
2017-03-23 22:39:37 +0000

    change prop inside component with click event

--- Section-8-Code/Section Code (Start)/src/components/UserDetail.vue
+++ Section-8-Code/Section Code (Start)/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
@@ -15,6 +16,9 @@
         methods: {
             switchName() {
                 return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = 'Aurimas';
             }
         }
     }
